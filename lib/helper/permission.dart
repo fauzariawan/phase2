a7931970c25fea53354f9a43ccb6758d2e7f5922
Fauza Riawan <umardastan2014@gmail.com>
2022-09-26 10:34:27 +0700

    phase2

---
+++ lib/helper/permission.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:location_permissions/location_permissions.dart';
+
+class Permission {
+  requestLocationPermission() async {
+    PermissionStatus permission =
+        await LocationPermissions().requestPermissions();
+    return permission;
+  }
+}
